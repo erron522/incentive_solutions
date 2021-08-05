2952b8c9d137b8598debed051d0531960a666742
Erron White <erron.white@gmail.com>
2021-08-05 08:53:08 -0500

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,90 @@
+
+
+window.onload = function() {
+
+    //Variables
+    let emailState = false;
+    let emailModal = document.getElementsByClassName('email-modal') [0];
+    let closeButton =   document.getElementsByClassName('email-modal__close-btn')[0];
+    let emailInput =   document.getElementsByClassName('email-modal__input')[0];
+    let emailButton =   document.getElementsByClassName('email-modal__button')[0];
+    let thankContainer = document.getElementsByClassName('email-thank') [0];
+    let declineOffer = document.getElementsByClassName('email-modal__decline-offer')[0];
+    
+    let showModal = () => {
+        if (emailState == false) {
+            emailModal.classList.add('email-modal--visible');
+            emailState = true   
+        }  
+    }
+
+
+    let closeModal = () => {
+        emailModal.classList.remove('email-modal--visible');
+    }
+
+    let addErrors = () => {
+        document.getElementsByClassName('email-modal__form-group') [0].classList.add('email-modal__form-group-error');
+        document.getElementsByClassName('email-modal__error-message') [0].classList.add('email-modal__error-message-active');
+    }
+
+    let removeErrors = () => {
+        document.getElementsByClassName('email-modal__form-group') [0].classList.remove('email-modal__form-group-error');
+        document.getElementsByClassName('email-modal__error-message') [0].classList.remove('email-modal__error-message-active');
+    }
+
+    let showThankMessage = () => {
+        thankContainer.classList.add('email-thank--success');
+        setTimeout(() => {
+            closeModal();
+        }, 3000)
+    }
+
+
+    //Email Info ----
+
+
+    //Validation
+    function emailIsValid (email) {   
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
+    }
+
+    //Add error message when not validated
+    emailButton.addEventListener('click', () => {
+        if(emailIsValid(emailInput.value)) {
+            showThankMessage();
+        }
+        else {
+           addErrors();
+        }
+    })  
+    
+    //Remove error message when validated
+    emailInput.addEventListener('click', () => {
+        removeErrors();
+    })
+
+
+    //Close Button 'X' button
+    closeButton.addEventListener('click', () => {
+        closeModal();
+    });
+
+    //Sorry, Im not interested "LINK"
+    declineOffer.addEventListener('click', () => {
+        closeModal();
+    });
+
+
+  
+
+    
+    document.body.addEventListener('mouseleave', () => {
+        showModal ();   
+    });
+    
+    console.log(document)
+    
+    
+    
+}
